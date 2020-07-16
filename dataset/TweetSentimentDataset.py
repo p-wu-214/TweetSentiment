ebabcd2cb2bbf61dba3d8306fda33854a4d12005
p-wu-214 <pattersonyou0214@gmail.com>
2020-07-15 19:29:53 -0700

    new model file

--- dataset/TweetSentimentDataset.py
+++ dataset/TweetSentimentDataset.py
@@ -3,8 +3,48 @@ from torch.utils.data import Dataset
 
 from sklearn.model_selection import KFold
 
+from transformers import RobertaTokenizerFast
+
 import pandas as pd
 import numpy as np
+
+OFFSET_FOR_ENCODING = 4
+MAX_LENGTH = 280+5
+tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
+
+def subfinder(mylist, pattern):
+    matches = []
+    for i in range(len(mylist)):
+        if mylist[i] == pattern[0] and mylist[i:i+len(pattern)] == pattern:
+            return i, i+len(pattern)
+
+def process_data(sentiment, text, selected_text):
+    start_index, end_index = subfinder(text, selected_text)
+    text_in_selected_text = [0] * len(text)
+    for idx in range(start_index, end_index):
+        text_in_selected_text[idx] = 1
+
+    token_text = tokenizer.encode_plus(sentiment, text, return_attention_mask=True,
+                                       return_token_type_ids=True, max_length=MAX_LENGTH,
+                                       pad_to_max_length=True, return_offsets_mapping=True,
+                                       truncation=True)
+    part_of_selected_text = []
+    # idx is the index of the word where the character was found to be part
+    # of selected_text. offset1 and offset2 are just of the original string encoded
+    # for each token. Eg) tokenizer.encode('I am the dog'), offset[2] = (2, 3). Note we do 3:-1
+    # because first few are <s>Sentiment</s></s> therefore we start after those useless parts
+    SKIP_USELESS_TOKENS = 4
+    for idx, (offset1, offset2) in enumerate(token_text['offset_mapping'][SKIP_USELESS_TOKENS:-1]):
+        # We do sum here because if any character is a 1, that means the whole word is a 1
+        if (offset2 - offset1) > 0 and sum(text_in_selected_text[offset1: offset2]) > 0:
+            part_of_selected_text.append(idx+SKIP_USELESS_TOKENS)
+    return {
+        'start': part_of_selected_text[0],
+        'end': part_of_selected_text[-1]+1,
+        'input_ids': token_text['input_ids'],
+        'mask': token_text['attention_mask'],
+        'token_type_ids': token_text['token_type_ids']
+    }
 
 def load_data():
     train = pd.read_csv('./data/train.csv')
@@ -13,7 +53,6 @@ def load_data():
     del train['selected_text']
     del train['textID']
     train.dropna(inplace=True)
-    # train['sentiment'] = train['sentiment'].map(dict(neutral=0, positive=1, negative=-1))
     return train, targets
 
 class TweetSentiment(Dataset):
@@ -22,7 +61,21 @@ class TweetSentiment(Dataset):
         self.max_length = max(len(s) for s in self.X['text'])
 
     def __getitem__(self, index):
-        return {'X': self.X.iloc[index], 'Y': self.Y.iloc[index], 'max_length': self.max_length}
+        X = self.X.iloc[index]
+        Y = self.Y.iloc[index]
+        obj = process_data(X['sentiment'].strip(), X['text'].strip(), Y.strip())
+        return {
+            'original_tweet': X['text'].strip(),
+            'sentiment':X['sentiment'].strip(),
+            'selected_sentence': Y.strip(),
+            'original_input_ids': obj['input_ids'],
+            'input_ids': torch.tensor(obj['input_ids'], dtype=torch.long),
+            'token_type_ids': torch.tensor(obj['token_type_ids'], dtype=torch.short),
+            'mask': torch.tensor(obj['mask'], dtype=torch.short),
+            'start': torch.tensor(obj['start'], dtype=torch.short),
+            'end': torch.tensor(obj['end'], dtype=torch.short),
+            'tokenizer': tokenizer
+        }
 
     def __len__(self):
         return len(self.X)
--- model.py
+++ model.py
@@ -3,61 +3,29 @@ from transformers import *
 
 from dataset.TweetSentimentDataset import TweetSentiment
 
-OFFSET_FOR_ENCODING = 4
-MAX_LENGTH = 280+5
-tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base', model_max_length=MAX_LENGTH, padding_side='right')
-
-def subfinder(mylist, pattern):
-    matches = []
-    for i in range(len(mylist)):
-        if mylist[i] == pattern[0] and mylist[i:i+len(pattern)] == pattern:
-            return i, i+len(pattern)
-
-def generate_indices(sentiment, text, selected_text):
-    start_index, end_index = subfinder(text, selected_text)
-    text_in_selected_text = [0] * len(text)
-    for idx in range(start_index, end_index):
-        text_in_selected_text[idx] = 1
-
-    token_text = tokenizer.encode_plus(sentiment, text, return_offsets_mapping=True)
-    part_of_selected_text = []
-    # idx is the index of the word where the character was found to be part
-    # of selected_text. offset1 and offset2 are just of the original string encoded
-    # for each token. Eg) tokenizer.encode('I am the dog'), offset[2] = (2, 3). Note we do 3:-1
-    # because first few are <s>Sentiment</s></s> therefore we start after those useless parts
-    SKIP_USELESS_TOKENS = 4
-    for idx, (offset1, offset2) in enumerate(token_text['offset_mapping'][SKIP_USELESS_TOKENS:-1]):
-        # We do sum here because if any character is a 1, that means the whole word is a 1
-        if (offset2 - offset1) > 0 and sum(text_in_selected_text[offset1: offset2]) > 0:
-            part_of_selected_text.append(idx+SKIP_USELESS_TOKENS)
-    return {
-        'start': part_of_selected_text[0],
-        'end': part_of_selected_text[-1]+1,
-        'encoding': token_text['input_ids']
-    }
-
 def train():
     train_dataset = TweetSentiment()
-    cls = '<s>'
-    sep = '</s>'
     for epoch in range(1):
         for batch_num, batch in enumerate(train_dataset):
             if (batch_num == 10):
                 break
-            X, Y = batch['X'], batch['Y']
-            obj = generate_indices(X['sentiment'], X['text'].strip(), Y.strip())
-            start = obj['start']
-            end = obj['end']
-            encoding = obj['encoding']
-            print('X:'+X['text'])
-            print('Y:'+Y)
-            # No clue why but when decoding it will include the space before when it is in middle of sentence or not with a character before it
-            # Eg) I'd have, retrieving d will be fine without space but retrieving have will include space before it
-            print('decoded:', tokenizer.decode(encoding[start:end]).strip())
-            print('encoding: ', encoding)
+            tokenizer = batch['tokenizer']
+            start, end = batch['start'], batch['end']
+            print('test: ', batch['selected_sentence'])
+            print('tokenized: ', tokenizer.decode(batch['original_input_ids'][start:end]))
 
 
 
+def test_start_end_index():
+    train_dataset = TweetSentiment()
+    for epoch in range(1):
+        for batch_num, batch in enumerate(train_dataset):
+            if (batch_num == 10):
+                break
+            tokenizer = batch['tokenizer']
+            start, end = batch['start'], batch['end']
+            print('test: ', batch['selected_sentence'])
+            print('tokenized: ', tokenizer.decode(batch['original_input_ids'][start:end]))
 
 if __name__ == '__main__':
     train()
